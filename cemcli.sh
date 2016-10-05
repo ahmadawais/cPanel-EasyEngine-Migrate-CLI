a2c3bd1db8c5e5539aaf8e7d604164267bad8f31
Ahmad Awais <mrahmadawais@gmail.com>
2016-10-05 06:58:41 +0500

    FIX: Style 💯

--- cemcli.sh
+++ cemcli.sh
@@ -12,6 +12,43 @@
 # @param $IS_SUBDOMAIN Is it a subdomain?
 # @param $SUBDOMAIN_FOLDER: The sub domain folder.
 
+# Colors.
+#
+# colors from tput
+# http://stackoverflow.com/a/20983251/950111
+# Num  Colour    #define         R G B
+# 0    black     COLOR_BLACK     0,0,0
+# 1    red       COLOR_RED       1,0,0
+# 2    green     COLOR_GREEN     0,1,0
+# 3    yellow    COLOR_YELLOW    1,1,0
+# 4    blue      COLOR_BLUE      0,0,1
+# 5    magenta   COLOR_MAGENTA   1,0,1
+# 6    cyan      COLOR_CYAN      0,1,1
+# 7    white     COLOR_WHITE     1,1,1
+#
+# tput setab [1-7] # Set the background colour using ANSI escape
+# tput setaf [1-7] # Set the foreground colour using ANSI escape
+# tput sgr0    # Reset text format to the terminal's default
+# tput bel     # Play a bell
+#
+# Usage:
+# red=`tput setaf 1`
+# green=`tput setaf 2`
+# r=`tput sgr0`
+# echo "${redb}red text ${gb}green text${r}"
+bb=`tput setab 0` #set background black
+bf=`tput setaf 0` #set foreground black
+gb=`tput setab 2` # set background green
+gf=`tput setab 2` # set background green
+blb=`tput setab 4` # set background blue
+blf=`tput setaf 4` # set foreground blue
+rb=`tput setab 1` # set background red
+rf=`tput setaf 1` # set foreground red
+wb=`tput setab 7` # set background white
+wf=`tput setaf 7` # set foreground white
+r=`tput sgr0`     # r to defaults
+
+# Main function.
 function cem_cli_init() {
 	clear
 	cd ~
@@ -20,30 +57,33 @@ function cem_cli_init() {
 	BACKUP_FILE=b.tar.gz
 
 	# $BACKUP_URL URL to publically downloadable .tar.gz cPanel Backup file.
+	# $BACKUP_URL URL to publically downloadable .tar.gz cPanel Backup file.
 	echo "——————————————————————————————————"
-	echo "-"
-	echo " ⚡️ CEM CLI — cPanel to EasyEngine Migration CLI"
-	echo "Version 1.1.0"
-	echo "Migrates cPanel websites to EasyEngine based self hosted VPS"
-	echo "-"
+	echo "${gb}${bf} CEM CLI ⚡️  ${r}"
+	echo "${wb}${bf} Version 1.1.0 ${r}"
+	echo "${wb}${bf} cPanel to EasyEngine Migration CLI${r}"
+	echo "——————————————————————————————————"
+
+	echo "${gb}${bf}  ℹ️  Pre CEM CLI Checklist: ${r}"
+	echo "${wb}${bf}  ␥  1. Have you installed EasyEngine? If not then do it (INFO: https://easyengine.io/docs/install/)? ${r}"
+	echo "${wb}${bf}  ␥  2. Did you install WPCLI from EasyEngine Stacks? (INFO: https://easyengine.io/docs/commands/stack/) ${r}"
+	echo "${wb}${bf}  ␥  3. Do you have a publically downloadable full backup of your cPanel? ${r}"
+	echo "${wb}${bf}  ␥  4. Do you have your site's DB Name, USER, PASS, and PREFIX? You can find this inside 'wp-config.php' file. ${r}"
+	echo "${wb}${bf}  ␥  5. Have you set EasyEngine to ask for DB Name, USER, PASS, and PREFIX? If not then do that by 'sudo nano /etc/ee/ee.conf' (INFO: https://easyengine.io/docs/config/) ${r}"
+	echo "${blb}${bf}  INFO: All the above steps above are required for CEM CLI to work. ${r}"
 	echo "——————————————————————————————————"
 
 	echo "-"
 	echo "——————————————————————————————————"
-	echo "-"
-	echo " ℹ️  Pre CEM CLI Checklist:"
-	echo " ␥  1. Have you installed EasyEngine? If not then do it (INFO: https://easyengine.io/docs/install/)?"
-	echo " ␥  2. Did you install WPCLI from EasyEngine Stacks? (INFO: https://easyengine.io/docs/commands/stack/)"
-	echo " ␥  3. Do you have a publically downloadable full backup of your cPanel?"
-	echo " ␥  4. Do you have your site's DB Name, USER, PASS, and PREFIX? You can find this inside 'wp-config.php' file."
-	echo " ␥  5. Have you set EasyEngine to ask for DB Name, USER, PASS, and PREFIX? If not then do that by 'sudo nano /etc/ee/ee.conf' (INFO: https://easyengine.io/docs/config/)"
-	echo "-"
-	echo " INFO: All the above steps above are required for CEM CLI to work."
-	echo "-"
+	echo "👉  Do you have the INFO required for CEM CLI to run? [ y | n ]:"
 	echo "——————————————————————————————————"
-	echo "-"
+	read -r INFO
 
-	echo "-"
+	if [[ "y" != "$INFO" || "Y" != "$INFO" ]]; then
+		echo "${rb}${wf}  ❌ Get the INFO and run CEM CLI again. ${r}"
+		exit 1
+	fi
+
 	echo "——————————————————————————————————"
 	echo "👉  Enter PATH to a publically downloadable cPanel backup [E.g. http://domain.ext/backup.tar.gz]:"
 	echo "——————————————————————————————————"
@@ -229,7 +269,7 @@ function cem_cli_init() {
 
 	else
 		echo "——————————————————————————————————"
-		echo "❌  Backup Download Failed 👎"
+		echo "${rb}${wf}  ❌  Backup Download Failed 👎 ${r}"
 		echo "——————————————————————————————————"
 		echo "ℹ️  TIP: Check if the backup URL you added is a publically downloadable .tar.gz file."
 		echo "-"
